6c1658d7621d2829c25b42434e30ecfa20c0fb9d
basma-ali91 <basmaali01@gmail.com>
2023-04-03 14:35:50 +0200

    fourthpyramid

--- js/main.js
+++ js/main.js
@@ -4,19 +4,39 @@ AOS.init({
 });
 
 // change bgcolor of navbar
-let nav = document.querySelector("#home nav");
-let contactnav=document.querySelector("#contactpage nav")
-window.addEventListener("scroll", function () {
-    let homeSection = document.getElementById("home");
-    let scrollNav = window.scrollY;
-    let homeHeight = homeSection.scrollHeight;
-    if (scrollNav > homeHeight) {
-        nav.style.backgroundColor = "rgba(0,0,0,.8)";
-    }
-    else{
-        nav.style.backgroundColor="rgb(74 73 58 / 73%)"
+// let nav = document.querySelector("#home nav");
+// let contactnav=document.querySelector("#contactpage nav")
+// window.addEventListener("scroll", function () {
+//     let homeSection = document.getElementById("home");
+//     let scrollNav = window.scrollY;
+//     let homeHeight = homeSection.scrollHeight;
+//     if (scrollNav > homeHeight) {
+//         nav.style.backgroundColor = "rgba(0,0,0,.8)";
+//     }
+//     else{
+//         nav.style.backgroundColor="rgb(74 73 58 / 73%)"
+//     }
+    
+// })
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    rtl:true,
+    margin:10,
+    dotsEach:true,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        768:{
+            items:2,
+        },
+        1024:{
+            items:3,
+        }
     }
     
 })
-
 
